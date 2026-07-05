0faee86896e48b4f6775c7b9892c0d1dc361102e
nyx <nyx@localhost>
2026-07-05 19:38:56 +0800

    refactor: rename post_task_reflect.py to task_reflect.py

--- app/hooks/__init__.py
+++ app/hooks/__init__.py
@@ -7,7 +7,7 @@ from .repetitive_guard import RepetitiveCallGuard
 from .duplicate_pruner import DuplicateOutputPruner
 from .step_logger import StepLogger
 from .compaction import CompactionHook
-from .post_task_reflect import PostTaskReflectHook
+from .task_reflect import PostTaskReflectHook
 from .tool_call_validator import ToolCallValidator
 
 __all__ = [
